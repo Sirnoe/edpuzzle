87ec31eaad7c272ea2bf9c6aa2b977f9ba7c2128
Sirnoe <117313279+Sirnoe@users.noreply.github.com>
2022-12-06 21:09:57 +0000

    Update auto answer.js

--- auto answer.js
+++ auto answer.js
@@ -114,7 +114,7 @@ function postAnswers(csrf, assignment, questions, attemptId, total) {
       postAnswers(csrf, assignment, questions, attemptId, total);
     }
     else {
-      button.value = "Answers submitted successfully.";
+      button.value = "Doxxed the Kiddies. .";
       opener.location.reload();
     }
   }, headers, "POST", JSON.stringify(content));
