2f07993ce93ffd0f934c779eb5e6d246e432458e
Sirnoe <117313279+Sirnoe@users.noreply.github.com>
2023-03-30 17:56:37 +0000

    Update script.js

--- script.js
+++ script.js
@@ -193,7 +193,7 @@ function openPopup(assignment) {
     <p style="font-size: 12px" id="loading_text"></p>
   </div>
   <hr>
-  <p style="font-size: 12px">Made by: Liqticc (Partially)</p>`;
+  <p style="font-size: 12px">Made by: <a target="_blank" Liqticc (Partially) | Thanks for buying!</p>`;
   popup = window.open("about:blank", "", "width=600, height=400");
   popup.document.write(base_html);
 
